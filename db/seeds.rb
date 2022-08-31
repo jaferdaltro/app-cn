44ad3738cb657fc5b1927eb0f4ce94861b9382a2
Jafer <jafer.daltro@gmail.com>
2022-08-31 06:35:17 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Field.create(name: 'não congregado')
+Field.create(name: 'CN Fortaleza' )
+Field.create(name: 'CN Eusébio' )
+Field.create(name: 'CN Maraponga' )
+Field.create(name: 'CN Messejana' )
+Field.create(name: 'CN On-Line' )
+Field.create(name: 'CN Natal' )
+Field.create(name: 'CN Recife' )
